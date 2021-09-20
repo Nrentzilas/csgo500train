040e0c21b58619a57d07621e0b758ec46a4638c1
Nrentzilas <67762842+Nrentzilas@users.noreply.github.com>
2021-09-20 14:54:33 +0000

    Update_v2 (Collab: Xanahs)
    ***UPDATES***
    
    Update_v2 Notes:
    
    1. Added a Cooldown timer until the next train join.
    2. Added a Conversion of the Bux Balance to Usd.
    
    Best Regards,
    Nrentzilas&Xanahs

--- csgo500train.py
+++ csgo500train.py
@@ -3,6 +3,9 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 import time
+import sys
+from subprocess import call
+
 
 
 
@@ -11,6 +14,7 @@ chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
 #Change chrome driver path accordingly
 chrome_driver = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(chrome_driver, options=chrome_options)
+
 
 
 driver.get("https://csgo500.com/wheel")
@@ -27,7 +31,14 @@ actions.click(trainicon)
 
 for i in range(500000):
       actions.perform()
-      print("Successfully joined the train!")
+      print('\n\nSuccessfully joined the train!\n')
       count = (bux_count.text)
-      print('Current Balance: ' + count +' Bux')
-      time.sleep(120)
+      int_count = driver.find_element_by_id("balance")
+      int_count = int(count.replace(',', ''))
+      onebuxprice = int(6)
+      bux_price = int()
+      bux_price = (int_count*onebuxprice)/10000
+      bux_price_str =str(bux_price)
+      print('Current Balance: ' + count +' Bux , In Usd: '+ bux_price_str+'\n')
+      call(["python", "Countdown.py"])
+time.sleep(1)
